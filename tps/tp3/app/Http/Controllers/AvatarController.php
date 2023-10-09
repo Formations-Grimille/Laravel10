848e7a8875b079e6fc2a542dcc2d77b5615c302d
Grimille <mickael.dhainaut@gmail.com>
2023-10-09 17:40:08 +0200

    Better code

--- tps/tp3/app/Http/Controllers/AvatarController.php
+++ tps/tp3/app/Http/Controllers/AvatarController.php
@@ -6,14 +6,9 @@ use Illuminate\Http\Request;
 
 class AvatarController extends Controller
 {
-    public function generateAvatar($firstname, $lastname, $size) {
-        if ($size > 256) {
-            $size = 256;
-        }
-
-        if ($size < 24) {
-            $size = 24;
-        }
+    public function generateAvatar(string $firstname, string $lastname, int $size) {
+        if ($size > 256) $size = 256;
+        if ($size < 24) $size = 24;
         
         return "<img src='https://ui-avatars.com/api/?name=$firstname+$lastname&size=$size'/>";
     }
