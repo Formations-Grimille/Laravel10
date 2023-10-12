ea27eac80a2f4078b64881717581338b99b5e480
Grimille <mickael.dhainaut@gmail.com>
2023-10-12 11:41:30 +0200

    TP-12

---
+++ tps/tp12/resources/views/products/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">Article 1</div>
+            <div class="col-3">Article 2</div>
+            <div class="col-3">Article 3</div>
+            <div class="col-3">Article 4</div>
+        </div>
+    </div>
+@endsection
---
+++ tps/tp12/resources/views/products/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3"><h1>Zoom produit.</h1></div>
+    </div>
+</div>
+
+@endsection
